64f756c30e8243b5fdee28d7e6e8a7fd58891fbf
Cristian Granada <cristiansordo11@gmail.com>
2020-11-12 22:29:01 -0500

    Clase 7

---
+++ clase7/Ejercicio7.js
@@ -0,0 +1,35 @@
+let numUno;
+let numDos;
+let numTres;
+
+function Calcular(){
+    numUno = Number(document.getElementById('NumUno').value);
+    numDos = Number(document.getElementById('NumDos').value);
+    numTres = Number(document.getElementById('NumTres').value);
+    numMayor(numUno,numDos,numTres);
+
+}
+
+
+function numMayor(num1,num2,num3){
+    if(num1>num2){
+        if(num1>num3){
+            alert('El número mayor es' +num1 );
+            
+        }
+        else{
+            alert('El número mayor es' +num3 );
+            console.log('El número mayor es' + num3);
+    
+            
+    }
+    else{
+        if(num2>num3){
+            alert('El número mayor es' +num2 );
+        }
+        else{
+            alert('El número mayor es' +num3 );
+        }
+
+    }
+}
